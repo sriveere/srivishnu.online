69761ccf1da0e62cc1e59c7211104260aeccdf9f
sri_wierdo <mannemsrivishnureddy@gmail.com>
2022-05-19 07:20:27 +0530

    Deployable Build Ready, footer section done, email server is setup with email.js workexperience section is set with swiper.js home site link added in package.jason

--- v2.0/src/components/footer/Footer.jsx
+++ v2.0/src/components/footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {BsFacebook} from'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+import {BsTwitter} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <div className="footer__backtotop">
+          <a href="#">Back to Top</a>
+        </div>
+        <div className="footer__copyright">
+          <small> &#169; This website is made by me.. but I'm keeping this Repo public...Use it If you want to </small>
+        </div>
+
+      <div className="footer__socials">
+        <a className='footer__logo' href="https://www.facebook.com/vishnugadikaburlu"><BsFacebook /></a>
+        <a className='footer__logo' href="https://www.instagram.com/sri_wierdo"><BsInstagram /></a>
+        <a className='footer__logo' href="https://www.twitter.com/sri_wierdo"><BsTwitter /></a>
+        <a className='footer__logo' href="https://www.github.com/sri-wierdo"><BsGithub /></a>      
+        </div>
+    </footer>
+
+
+
+
   )
 }
 
